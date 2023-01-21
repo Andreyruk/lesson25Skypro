ffbcd38f9ffe808223882e239ef8738b2fb19aaa
MakTub <7andreyru@mail.ru>
2023-01-22 03:45:56 +0500

    ДЗ к уроку 25

---
+++ src/main/java/pro/sky/lesson25skypro/Lesson25SkyproApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson25skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson25SkyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson25SkyproApplication.class, args);
+	}
+
+}
